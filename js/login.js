2ade35436aa216ef56047b045e8c8b7caa7fcb34
Gianluca <clabitomello@gmail.com>
2021-08-19 23:16:27 -0300

    entrega 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,54 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+function verificar(){
+let user = document.getElementById('user')
+let password = document.getElementById('pass')
+let msj = document.getElementById('msj')
+let users = {}
+
+if(user.value.trim() === '' || password.value.trim() === ''){
+    //Valido - Invalido
+    user.classList.add('invalido');
+    password.classList.add('invalido');
+    
+    msj.innerHTML='Tu usuario o contraseña son incorrectos';
+    msj.style.color='red';
+    msj.style.display='block';
+} else{
+    
+    location.href ='index.html';
+
+    users.nombre = user.value;
+    users.estado = 'conectado';
+
+    localStorage.setItem('users', JSON.stringify(users));
+    sessionStorage.setItem('users', JSON.stringify(users));
+}
+
+}   
+
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    let users =  JSON.parse( localStorage.getItem('users'))
+    if(users.estado == 'conectado'){
+        location.href = 'index.html'}   
+    
 
 });
+
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      }
