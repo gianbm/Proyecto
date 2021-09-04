38fbb9e7db43c8f44deaeef653c03b93943b2d76
gianbm <88291431+gianbm@users.noreply.github.com>
2021-09-03 23:56:59 -0300

    a

--- js/products.js
+++ js/products.js
@@ -2,44 +2,49 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
-var categoriesArray = [];
 
-function showCategoriesList(array){
-    let htmlContentToAppend = "";
-    for(let i = 0; i < array.length; i++){
-        let category = array[i];
+let productsArray = [];
 
-        htmlContentToAppend += `
-        <div class="list-group-item list-group-item-action w-100% mt-1 ">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ category.name +`</h4>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small>
-                    </div>
-                        <p class="mb-1">` + category.description + `</p>
-                </div>            
-            </div>
-            <p class="precio">Precio:` + category.cost + `</p>   
-        </div>
-        `
+function showProductsList(array){
 
-        document.getElementById("productos").innerHTML = htmlContentToAppend;
+    if(array == undefined){
+        array = productsArray;
     }
+
+    let minimo = document.getElementById('minimo').value;
+    let maximo = document.getElementById('maximo').value;
+
+    let htmlContentToAppend = "";   
+
+    for(let i = 0; i < array.length; i++){
+        let products = array[i];
+        if( products.cost >= minimo && products.cost <= maximo){
+
+            htmlContentToAppend += `
+            <div class="list-group-item list-group-item-action w-100% mt-1 ">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ products.name +`</h4>
+                            <small class="text-muted">` + products.soldCount + ` artículos</small>
+                        </div>
+                            <p class="mb-1">` + products.description + `</p>
+                    </div>            
+                </div>
+                <p class="precio">Precio:` + products.cost + `</p>   
+            </div>
+            `
+
+            document.getElementById("productos").innerHTML = htmlContentToAppend;
+        } 
+    } 
     
 }
 
-function ornderPorPrecio(){
-    let min = document.getElementById('min')
-    let max = document.getElementById('max')
 
-    if ( min == undefined || min != undefined && parseInt(category.cost) >= min){
-
-    }
-}
 
 
 
@@ -52,11 +57,11 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
-            categoriesArray = resultObj.data;
+            productsArray = resultObj.data;
             //Muestro las categorías ordenadas
-            showCategoriesList(categoriesArray);
+            showProductsList(productsArray);
         }
     });
-});
 
+});
 
