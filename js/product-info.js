e41f48c7914f02584ca74a6a1af59182b67eb975
gianbm <88291431+gianbm@users.noreply.github.com>
2021-09-10 13:09:34 -0300

    a

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,27 @@
+let info = [];
+
+function mostrarInfo(inf){
+    
+    document.getElementById('infoProducts').innerHTML = 
+    `<div class='prodInfo'> 
+        <div>`+ inf.name +`</div>
+
+    </div>`
+};
+
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if(resultObj.status === 'ok'){
+            info = resultObj.data;
+            mostrarInfo(info)
 
+        }
+    })
 });
